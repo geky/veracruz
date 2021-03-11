eb0de5b85bcc7b710bb7131452206c5ba793ddbe
Christopher Haster <chaster@utexas.edu>
2021-03-10 19:08:26 -0600

    Integrated the diesel migrations into the PAS itself
    This avoids needing to run populate-test-database.sh to create
    an initial database. However, this raises a question of if it's
    correct behavior for the PAS to include some fixed hash of the root
    enclaves.
    
    I'm currently wondering if a better alternative would be to provide the
    binaries/hash to the PAS during setup.
    
    Need to revisit.

--- proxy-attestation-server/build.rs
+++ proxy-attestation-server/build.rs
@@ -9,20 +9,57 @@
 //! See the `LICENSE.markdown` file in the Veracruz root directory for
 //! information on licensing and copyright.
 
+#[cfg(feature = "sgx")]
 use std::process::Command;
+#[cfg(feature = "sgx")]
+use hex;
+#[cfg(feature = "nitro")]
+use std::fs;
 
 fn main() {
-    // TODO: Ideally, the following whould only be run for tests.
-    // However, cargo doesn't currently support that.
-    // https://github.com/rust-lang/cargo/issues/4001
-    {
-        println!("cargo:rerun-if-changed=build.rs");
-        println!("cargo:rustc-link-search=/usr/lib/aarch64-linux-gnu");
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rustc-link-search=/usr/lib/aarch64-linux-gnu");
 
-        // Destroy, and then re-create and repopulate, the database
-        Command::new("bash")
-            .args(&["./populate-test-database.sh"])
-            .output()
-            .unwrap();
+    // grab hashes from the root enclaves
+    #[cfg(feature = "sgx")]
+    {
+        println!("cargo:rerun-if-changed=../trustzone-root-enclave/css.bin");
+        println!("cargo:rustc-env=SGX_FIRMWARE_VERSION=0.3.0");
+        println!(
+            "cargo:rustc-env=SGX_FIRMWARE_HASH={}",
+            hex::encode(
+                Command::new("dd")
+                    .args(&[
+                        "skip=960",
+                        "count=32",
+                        "if=../trustzone-root-enclave/css.bin",
+                        "bs=1",
+                        "status=none"])
+                    .output()
+                    .unwrap()
+                    .stdout
+            ),
+        );
+    }
+
+    #[cfg(feature = "psa")]
+    {
+        println!("cargo:rustc-env=PSA_FIRMWARE_VERSION=0.3.0");
+        println!(
+            "cargo:rustc-env=PSA_FIRMWARE_HASH={}",
+            "deadbeefdeadbeefdeadbeefdeadbeeff00dcafef00dcafef00dcafef00dcafe"
+        );
+    }
+
+    #[cfg(feature = "nitro")]
+    {
+        println!("cargo:rerun-if-changed=../nitro-root-enclave/PCR0");
+        println!("cargo:rustc-env=NITRO_FIRMWARE_VERSION=0.1.0");
+        println!(
+            "cargo:rustc-env=NITRO_FIRMWARE_HASH={}",
+            fs::read("../nitro-root-enclave/PCR0")
+                .unwrap()
+                .stdout
+        );
     }
 }
